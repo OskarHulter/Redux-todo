006974c218516bf7e83f82dadbce6900e269bba8
OskarHulter <stresslessness91@gmail.com>
2018-08-24 17:15:30 +0200

    First test passing =)

---
+++ index.test.js
@@ -0,0 +1,28 @@
+import deepFreeze from 'deep-freeze';
+import todos from './index';
+
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learning Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learning Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    test('Added todo without mutation', () => {
+        expect(todos(stateBefore, action)).toEqual(stateAfter);
+    });
+};
+
+testAddTodo();
+console.log('Well Played, Tests passing!');
